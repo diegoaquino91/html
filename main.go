eec400ecb38cc62583ba24f37994fa5865881a4a
Diego Aquino <110973873+diegoaquino91@users.noreply.github.com>
2024-06-28 00:16:54 -0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,25 @@
+package html
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"regexp"
+)
+
+// Titulo obtem o título de uma pagina html
+func Titulo(urls ...string) <-chan string {
+	c := make(chan string)
+
+	for _, url := range urls {
+		go func(url string) {
+			resp, _ := http.Get(url)
+			html, _ := io.ReadAll(resp.Body)
+
+			r, _ := regexp.Compile("<title>(.*?)</title>")
+			c <- r.FindStringSubmatch(string(html))[1]
+		}(url)
+	}
+
+	return c
+}
